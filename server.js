ef7a41240c10538911d9f22239f59734789f1458
Osama Baker <edewanjy@hotmail.com>
2021-08-25 11:00:45 +0300

    Initial project prep

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// error handlers
+const notFoundHandler = require('./src/error-handlers/404');
+const errorHandler = require('./src/error-handlers/500');
+
+const acl = require('./src/middleware/acl');
+const basicAuth = require('./src/middleware/basic-auth');
+const bearerAuth = require('./src/middleware/bearer-auth');
+
+app.use(express.json());
+
+
+
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+    app: app,
+    start: (PORT) => {
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`)
+        })
+    }
+}
